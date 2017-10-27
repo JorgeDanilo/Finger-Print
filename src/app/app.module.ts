64759a77a6b0b4b82be3fcc311f0df612365edcf
JorgeDanilo <sobrejavatec@gmail.com>
2017-10-27 09:45:30 -0200

    project test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -22,6 +24,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    FingerprintAIO,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public aio: FingerprintAIO) {
 
+  }
+
+  check() {
+    this.aio.show({
+      clientId: 'fingerauth',
+      clientSecret: 'password', //Only necessary for Android
+      disableBackup:true,  //Only for Android(optional)
+      localizedFallbackTitle: 'Use Pin', //Only for iOS
+      localizedReason: 'Please authenticate' //Only for iOS
+    })
+      .then((result: any) => console.log(result))
+      .catch((error: any) => console.log(error));
   }
 
 }
