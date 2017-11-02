ee6c8eac267a7178a9b258ef2bc53ba012d34fe4
JorgeDanilo <sobrejavatec@gmail.com>
2017-11-02 10:22:23 -0200

    teste img base 64 image crop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+import { Camera } from '@ionic-native/camera';
+import { Crop } from '@ionic-native/crop';
+import { Base64 } from '@ionic-native/base64';
 
 
 import { MyApp } from './app.component';
@@ -25,6 +28,9 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     FingerprintAIO,
+    Crop,
+    Camera,
+    Base64,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
 import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+import { Crop } from '@ionic-native/crop';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Base64 } from '@ionic-native/base64';
+
 
 @Component({
   selector: 'page-home',
@@ -8,9 +12,19 @@ import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public aio: FingerprintAIO) {
+
+  imageSrc: any;
+
+  constructor(
+        public navCtrl: NavController,
+        public aio: FingerprintAIO,
+        public camera: Camera,
+        public platform: Platform,
+        public base64: Base64,
+        public crop: Crop) {
 
   }
+
 
   check() {
     this.aio.show({
@@ -23,5 +37,43 @@ export class HomePage {
       .then((result: any) => console.log(result))
       .catch((error: any) => console.log(error));
   }
+
+
+  openGallery() {
+
+    const options: CameraOptions = {
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      quality: 30,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.ALLMEDIA,
+      correctOrientation: true
+    };
+
+    this.camera.getPicture(options)
+      .then((fileUri) => {
+      if ( this.platform.is('ios') ) {
+        return fileUri;
+      } else if ( this.platform.is('android') ) {
+        fileUri = 'file://' + fileUri;
+      }
+
+      return this.crop.crop(fileUri, {quality: 30});
+
+    }).then( (path) => {
+      console.log('Cropped Image Path!: ' + path);
+      this.base64.encodeFile(path).then((base64File :string) => {
+        console.log(base64File);
+      }, (err) => {
+        console.log(err);
+      });
+      return path;
+    })
+  }
+
+
+
 
 }
